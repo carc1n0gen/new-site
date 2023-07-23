1e24ce16747616d4bf44e43dfe2c019dfee0d356
Carson Evans <carsonwevans@gmail.com>
2023-07-23 17:37:38 -0400

    use card component on not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,8 +1,10 @@
+import Card from "@/components/Card";
+
 export default function NotFound() {
   return (
-    <section className="mt-6 p-6 bg-white border border-zinc-300">
+    <Card>
       <h1 className="font-bold text-3xl mb-4">Not Found</h1>
       <p>Are you lost? There doesn't seem to be anything here</p>
-    </section>
+    </Card>
   );
 }
